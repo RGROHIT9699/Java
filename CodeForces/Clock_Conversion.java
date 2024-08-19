5df92f12b312fff65f96c1140fdd7d583ab5ff79
RGROHIT9699 <119799265+RGROHIT9699@users.noreply.github.com>
2024-08-19 23:59:22 +0530

    main

--- CodeForces/Clock_Conversion.java
+++ CodeForces/Clock_Conversion.java
@@ -1,27 +1,34 @@
 import java.util.Scanner;
-
 public class Clock_Conversion {
     public static void main(String[] args) {
-        Scanner qw = new Scanner(System.in);
-        int t = qw.nextInt();
-        while (t-- > 0) {
-            String str = qw.next();
-            int h = Integer.parseInt(str.substring(0, 2));
-            int m = Integer.parseInt(str.substring(3, 5));
-            int cou = 0;
-            if(h>=12){
-                cou++;
-            } 
-            String s = "AM";
-            if(cou == 1 ){
-                s = "PM";
-            }
-            h = h % 12;
-            if(h == 0){
-                h = 12;
-            } 
-            String ans = h + ":" + m + " " + s;
-            System.out.println(ans);
+        Scanner in= new Scanner(System.in);
+        int numberOfTEst= in.nextInt();
+        String arr[]= new String[numberOfTEst];
+        for(int i=0; i<numberOfTEst; i++){
+            arr[i]=in.next();
         }
+        for(int i=0; i<numberOfTEst; i++){
+            String[] s=arr[i].split(":");
+            int hh=Integer.valueOf(s[0]);
+            if(hh==0){
+                System.out.println("12:"+s[1]+" AM");
+            }
+            else if(hh<12){
+                System.out.println(arr[i]+" AM");
+            }
+            else if(hh==12){
+                System.out.println(arr[i]+" PM");
+            }
+            else{
+                int a=hh-12;
+                if(a<10){
+                    System.out.println("0"+a+":"+s[1]+" PM");
+                }
+                else{
+                    System.out.println(a+":"+s[1]+" PM");
+                }
+            }
+        }
+        in.close();
     }
 }
